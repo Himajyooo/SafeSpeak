af27c835c73fc329d8555921003afbf2d520479d
Sameera-18 <sameeratu341@gmail.com>
2024-03-29 14:43:44 +0530

    email validation

--- signup.py
+++ signup.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import mysql.connector
 from login import create_connection
+from email_validator import validate_email, EmailNotValidError
 
 def display_signup_page():
     st.title("Sign Up")
@@ -16,13 +17,22 @@ def display_signup_page():
     if st.button("Sign Up"):
         L = (username,password1,email)
         con,cur=create_connection()
-        strsql="insert into login (username,pass,email) values(%s,%s,%s)"
-        cur.execute(strsql,L)
-        con.commit()
-        con.close()
-        
-        st.success(f"Sign up successful! Username: {username}, Email: {email}, Password: {password1}")
+        # Check if the email format is valid
+        try:
+            # Validate the email using email-validator library
+            valid = validate_email(email)
+            strsql="insert into login (username,pass,email) values(%s,%s,%s)"
+            #st.success("Valid email format!")
+            cur.execute(strsql,L)
+            con.commit()
+            con.close()
+            
+            st.success(f"Sign up successful! Username: {username}, Email: {email}, Password: {password1}")
 
+        except EmailNotValidError as e:
+            st.error("Invalid email format. Please enter a valid email address.")
+        
+        
         # Redirect to login page
     if st.button("Go back"):
         st.session_state.logged_in = False
