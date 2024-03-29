ac973f335f63a4a5e091c80b2dcc7b7d9f93880b
Himajyooo <himajyothish123@gmail.com>
2024-03-29 14:17:38 +0530

    signupdb

--- signup.py
+++ signup.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import mysql.connector
+from login import create_connection
 
 def display_signup_page():
     st.title("Sign Up")
@@ -8,15 +10,23 @@ def display_signup_page():
     # Input fields for username, email, and password
     username = st.text_input("Username")
     email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
+    password1 = st.text_input("Password", type="password")
+    #create_connection()
     # Sign up button
     if st.button("Sign Up"):
+        L = (username,password1,email)
+        con,cur=create_connection()
+        strsql="insert into login (username,pass,email) values(%s,%s,%s)"
+        cur.execute(strsql,L)
+        con.commit()
+        con.close()
         
-        st.success(f"Sign up successful! Username: {username}, Email: {email}, Password: {password}")
+        st.success(f"Sign up successful! Username: {username}, Email: {email}, Password: {password1}")
 
         # Redirect to login page
+    if st.button("Go back"):
         st.session_state.logged_in = False
         #st.session_state.page = "user"
+        st.session_state.signup = False
         st.rerun()
-display_signup_page() 
+    
