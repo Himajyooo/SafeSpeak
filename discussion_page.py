aaf632c14cd8e35a7d53ea74c37361bc9e74d9a3
Sameera-18 <sameeratu341@gmail.com>
2024-04-07 13:48:23 +0530

    comment displaying

--- discussion_page.py
+++ discussion_page.py
@@ -1,6 +1,19 @@
 import streamlit as st
 from database import create_connection
 # Function to display discussion page
+def display_comments(con, cur):
+    sql = "SELECT l.username, c.comment_desc FROM comment c JOIN login l ON c.userid = l.userid JOIN discussion d ON c.discussion_id = d.discussion_id WHERE d.d_name ='discussion1'"
+    cur.execute(sql)
+    q=cur.fetchall()
+    print(q)
+    if len(q)==0:
+        st.write("No comments posted yet")
+    else:
+        i=0
+        for comments in q:
+            st.write(f" {q[i][0]} :  {q[i][1]}")
+            i+=1
+                
 def display_discussion_page():
     con,cur =  create_connection()
     d_name=st.session_state.selected_discussion
@@ -12,30 +25,20 @@ def display_discussion_page():
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
     user_id=st.session_state.user_id
 
-
-    # Display comments for the selected discussion (you can retrieve comments from your database)
-    #print(selected_discussion)
     if d_name:
         #st.write("Here is the content of the selected discussion.")
         st.header("Comments")
-        d_name=tuple(d_name)
-        #cur.execute("select username, comment_desc from comment,login where discussion_id=%s",d_name)
-        
-
-        # Placeholder for displaying comments
-        # comments = []  # List to store comments, assuming comments are retrieved from a database
-        # # Display existing comments
-        # for comment in comments:
-        #     st.write(comment)
-        # # Comment submission form
+        display_comments(con,cur)
         
         new_comment = st.text_area("Write your comment here")
         if st.button("Post Comment"):
             # Placeholder for submitting the comment to your database
+            user_name = st.session_state.username
             cur.execute("insert into comment (discussion_id,comment_desc,userid) values (%s,%s,%s)",(d_id,new_comment,user_id))
             con.commit()
             st.success("Comment posted successfully")
-            #comments.append(new_comment)    
+
+            st.rerun() 
         if st.button("Back"):
             st.session_state.selected_discussion = None
             st.session_state.page = "user"
