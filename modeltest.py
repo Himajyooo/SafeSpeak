4bd6c409e83f5dd3893a14dc7d8f8f002a79010a
Himajyooo <himajyothish123@gmail.com>
2024-04-12 09:24:16 +0530

    modeltest

---
+++ modeltest.py
@@ -0,0 +1,14 @@
+from transformers import BertForSequenceClassification, BertTokenizer
+import torch
+
+# Load saved model and tokenizer
+#model = BertForSequenceClassification.from_pretrained('h:\modeltest.py')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+new_comment = "that was good point"
+# new_comment = "go to hell"
+encoded_comment = tokenizer(new_comment, padding='max_length', truncation=True, max_length=128, return_tensors='pt')
+output = model(**encoded_comment)
+probabilities = torch.nn.functional.softmax(output.logits, dim=-1)
+predicted_label = torch.argmax(probabilities, dim=-1)
+
+print("Predicted Label:", predicted_label.item())
